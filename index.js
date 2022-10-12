f3c1fa49ba5a57931e587ddc98856eef06042027
Henningham22 <Henningham22@hotmail.com>
2022-10-12 16:34:10 +0100

    react work 1

--- index.js
+++ index.js
@@ -23,3 +23,5 @@ app.use((err, req, res, next) => {
 })
 
 const server = app.listen(4002, () => console.log(`Server successfully started on port ${server.address().port}`));
+
+module.exports = server;
---
+++ react-work/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT44XB7zvG6Tsm6-93Br0H1kS37PVdWhkRTHw&usqp=CAU"  className="App-logo" alt="logo" />
+        <p>
+          LOS SANTOS CUSTOMS<br>
+          
+          
+          
+          
+          </br>  <br>
+          </br>WE PUT THE BLING IN RIMS.<br></br><code>src/App.js</code>
+          </p>
+          <p><img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuU3LX8tBteCfExsAOcmJIQSJTYougUYY03OlnaMfHIVPIUtLZGrIH-gxd-Z44iy2cAeI&usqp=CAU"></img>
+        </p>
+        <a
+          className="App-link"
+          href="https://gta.fandom.com/wiki/Los_Santos_Customs_Mechanic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          COME CHECK US OUT
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
